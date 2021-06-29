decc6be1bc8fd4c45bfbc314aaaa84171eeb48b7
Raj Kishore Sahni <rajkishoresahni826@gmail.com>
2021-06-29 19:25:38 +0530

    To display Hello From Raj Using data binding

--- src/App.js
+++ src/App.js
@@ -4,10 +4,18 @@ import './App.css';
 import React from 'react';
 class App extends React.Component{
 
+  constructor() {
+    super();
+    this.state= {
+      title : 'Hello from raj'
+    }
+  }
+  
+
   render() {
     return (
       <div> 
-        <h1>Hello World</h1>
+        <h1>{this.state.title}</h1>
       </div>
     );
   }
